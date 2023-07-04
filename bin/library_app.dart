bb06f4d2e65a2e7ebd35e95b7b1ea0142f64321b
Edson Gonzalez <edsongonzalez@infojr.com.br>
2023-07-04 16:36:26 -0300

    fix: use singleton

--- bin/library_app.dart
+++ bin/library_app.dart
@@ -1,19 +1,16 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:library_app/books/copy.dart';
 import 'package:library_app/command_interpreter.dart';
-import 'package:library_app/library_system.dart';
 
 void main(List<String> arguments) {
-  LibrarySystem system = LibrarySystem.instance;
-  CommandInterpreter commandInterpreter = CommandInterpreter();
+  CommandInterpreter interpreter = CommandInterpreter.instance;
   while (true) {
     String line = stdin.readLineSync(encoding: utf8) ?? '';
     if (line == 'sai') {
       print('Saindo do sistema...');
       break;
     }
-    commandInterpreter.interpretCommand(line);
+    interpreter.interpretCommand(line);
   }
 }
